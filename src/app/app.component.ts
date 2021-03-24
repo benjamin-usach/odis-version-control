1081f29d4138209fd5ca326c6c3834efb96f60d5
benjamin-bravo <benjamin.bravo@usach.cl>
2021-03-24 10:11:43 -0300

    firestore service add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { FirebaseService } from './services/firebase.service';
 
 @Component({
   selector: 'app-root',
@@ -12,56 +8,27 @@ import { map } from 'rxjs/operators';
 })
 
 export class AppComponent{
-  title = 'versionHistory';
+  title = 'versionHistory';  
 
-  public documentos$! : Observable<any[]>;
-  private documentosCollection!: AngularFirestoreCollection<any>;
-
-
-  
-
+  obj = {
+    categoria: "jICcX0aawQh2Cm4eMsNS",
+    descripcion: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure voluptas eligendi magni, fugit quidem quaerat. Labore ratione, obcaecati",
+    id: "WmtcOn5rINS3CKfOZUBg",
+    ver_creado_por: "benjamin.bravo",
+    ver_editado_por: "benjamin.bravo",
+    ver_number: "1.0.0",
+  }
 
   constructor(
-    private _afs: AngularFirestore,
-    //private firebaseAuth: AngularFireAuth
+    private fb: FirebaseService
   ) {}
 
   ngOnInit(){
-    console.log("Hola mundo");
-
-    this.documentosCollection = this._afs.collection(
-      "version"
-    );
-
-    this.documentos$ = this.documentosCollection.snapshotChanges().pipe(
-      map( actions =>
-        actions.map( a => {
-          //console.log(a);
-          const data = a.payload.doc.data();
-          const id = a.payload.doc.id;
-          console.log({ id, ...data });
-          return { id, ...data };
-        })
-      )
-    );
     
-    
-
-    this.documentos$.subscribe(resp => console.log(resp));
 
   }
-
   agregar(){
-    this._afs.collection("version").add(
-      {
-      categoria: "jICcX0aawQh2Cm4eMsNS",
-      descripcion: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure voluptas eligendi magni, fugit quidem quaerat. Labore ratione, obcaecati",
-      id: "WmtcOn5rINS3CKfOZUBg",
-      ver_creado_por: "benjamin.bravo",
-      ver_editado_por: "benjamin.bravo",
-      ver_number: "1.0.0"
-      }
-    );
+    this.fb.postCollectionFb("version", this.obj);
   }
 
 }
---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FirebaseService {
+
+  constructor(private _afs: AngularFirestore) {
+  }
+
+
+  getCollectionfb(ruta: string): Observable<any> {
+    let documentCollection = this._afs.collection(ruta);
+    return documentCollection.snapshotChanges().pipe(
+      map(actions =>
+        actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, data };
+        })
+      )
+    )
+  }
+
+  /** 
+   * function: postCollectionFB
+   * @param ruta :    Ruta en la que se va a agregar un objeto
+   * @param objeto:   Objeto a agregar 
+   * 
+   * returns: VOID
+   * 
+   */
+
+
+  postCollectionFb( ruta: string, objeto: Object ): void {
+    
+    this._afs.collection(ruta).add(objeto)
+      .then(
+        resp => console.log(resp)
+      )
+      .catch(err=> console.log(err))
+  } 
+
+}
